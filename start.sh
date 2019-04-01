4628b8b0d4b51cb5cdb29fc20b29affd46e255b3
favsol26 <favsol26@gmail.com>
2019-04-01 07:02:39 +0200

    do1819-02

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-
-bash stop.sh
-
 docker build -t load-balance-nginx  .
 docker run -p 9090:80 -d --name nginx load-balance-nginx
 
@@ -8,7 +5,7 @@ num=9091+$n
 
 for (( i=9091; i<num; i++ ))
 do
-docker run -dt -p $i:80 --cpus="0.25" --memory="128m" --memory-swap="128m" --name tsp-api$i favsol26/tspsolver-api
+docker run -dt -e mongoDBHostname=172.17.0.2 -p $i:8080 --cpus="0.25" --memory="128m" --memory-swap="128m" --name acme-api$i favsol26/do1819-02
 done
 
 docker logs nginx --follow
--- stop.sh
+++ stop.sh
@@ -1,1 +1,10 @@
-docker rm $(docker ps -aq) -f
+# docker rm $(docker ps -aq) -f
+
+num=9091+$n
+
+for (( i=9091; i<num; i++ ))
+do
+docker rm acme-api$i -f
+done
+
+docker rm nginx -f 
